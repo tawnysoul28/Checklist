504a9721045d5b89fed858541db9556aa3e4698b
Bob <tawnysoul@MacBook-Pro-Bob.local>
2019-09-15 19:56:29 +0700

    added priorities

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class ChecklistViewController: UITableViewController {
 
     var todoList: TodoList
-    var tableData: [[CheckListItem?]?]! //потому что это полюбому заполним в viewDidload. структура массива состоит в том, что для каждой буквы в алфавите будет свой чеклист айтем, НА случай если не будет чеклист айтема с опредленной буквой для списка букв (а,б,в,г...), делаем optional
+    
+    private func priorityForSectionIndex(_ index: Int) -> TodoList.Priority? { //переводит число индекса в енум, который в TodoList лежит
+        return TodoList.Priority(rawValue: index)
+    }
     
     @IBAction func addItem(_ sender: Any) {
         
-        let newRowIndex = todoList.todos.count // новый индекс под номером = то кол-во элементов в массиве.(потому что кол-во элементов всегда +1 от кол-ва индексов). т е в данном случае newRowIndex = 5 - это индекс нашей будущей ячейки.
+        let newRowIndex = todoList.todoList(for: .medium).count // новый индекс под номером = то кол-во элементов в массиве.(потому что кол-во элементов всегда +1 от кол-ва индексов). т е в данном случае newRowIndex = 5 - это индекс нашей будущей ячейки. автоматом в .medium
         _ = todoList.newTodo() // мы не будем использовать новый созданный айтэм. мы его просто создали и всё. i'm not interested in working with the object returned from this method.
         
         let indexPath = IndexPath(row: newRowIndex, section: 0)
@@ -27,11 +30,15 @@ class ChecklistViewController: UITableViewController {
     
     @IBAction func deleteItems(_ sender: Any) {
         if let selectedRows = tableView.indexPathsForSelectedRows {// selectedRows - это выбранные ячейки типа [[0-section, 2-row], [0,4]]
-            var items = [CheckListItem]() //массив чеклист айтемов, которые в конце удалим
             for indexPath in selectedRows {
-                items.append(todoList.todos[indexPath.row]) //добавляем в массив из айтемов те айтемы, который выбрал юзер путем нажатия на круглешок.
+                if let priority = priorityForSectionIndex(indexPath.section) {
+                    let todos = todoList.todoList(for: priority)
+                    
+                    let rowToDelete = indexPath.row > todos.count - 1 ? todos.count - 1 : indexPath.row
+                    let item = todos[rowToDelete] //добавляем в массив из айтемов те айтемы, который выбрал юзер путем нажатия на круглешок.
+                    todoList.remove(item, from: priority, at: rowToDelete)//ЭТО В МОДЕЛЕ.с помощью метода удаления, удаляем те айтемы, которые скопились в массиве сверху; это обновит нашу модель.
+                }
             }
-            todoList.remove(items: items) //ЭТО В МОДЕЛЕ.с помощью метода удаления, удаляем те айтемы, которые скопились в массиве сверху; это обновит нашу модель.
             tableView.beginUpdates()
             tableView.deleteRows(at: selectedRows, with: .automatic)//ЭТО В ТЭЙБЛВЬЮ. удаляем выбранные rows.
             tableView.endUpdates()
@@ -50,34 +57,18 @@ class ChecklistViewController: UITableViewController {
         navigationItem.leftBarButtonItem = editButtonItem//этими заклинаниями вставили в верхний левый угол Edit кнопку
         tableView.allowsMultipleSelectionDuringEditing = true //разрешаем выделять несколько rows во время редактирования
         
-        let sectionTitleCount = UILocalizedIndexedCollation.current().sectionTitles.count //кол-во букв, используемые здесь
-        var allSections = [[CheckListItem?]?](repeating: nil, count: sectionTitleCount) //инициализирует массив -[[CheckListItem?]?]. Это создаст 26 индивидуальных секций.
-        var sectionNumber = 0 //изначально кол-во секций 0
-        let collation = UILocalizedIndexedCollation.current()
-        for item in todoList.todos {
-            sectionNumber = collation.section(for: item, collationStringSelector: #selector(getter: CheckListItem.text))
-            if allSections[sectionNumber] == nil { //проверяем представлен ли здесь массив.
-                 allSections[sectionNumber] = [CheckListItem?]() //если нет, то создаем массив
-            }
-            allSections[sectionNumber]!.append(item) //assign it
-        }
-        tableData = allSections //assign it back to our table data
     }//ЭТО ИЗМЕНЯЕМ МОДЕЛЬ. вся эта херня сортирует всё в массивы(секции) (в алфавитном порядке по буквам видимо)
     
     override func setEditing(_ editing: Bool, animated: Bool) { // метод для редактирования????
         super.setEditing(editing, animated: true) //editing - так надо, не объясняется.
         tableView.setEditing(tableView.isEditing, animated: true)// y tableView есть метод isEditing.
     }
-    
-    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) { //этот метод позволяет двигать РЯДЫ
-        todoList.move(item: todoList.todos[sourceIndexPath.row], to: destinationIndexPath.row)//откуда и куда перемещаем
-        tableView.reloadData()
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData[section] == nil ? 0 : tableData[section]!.count //если нет , то вернем 0, если есть, то вернем кол-во секций
-        
-        //todoList.todos.count //подсчитать кол-во элем-ов в массиве, столько и будет рядов.(раньше было)
+        if let priority = priorityForSectionIndex(section) {
+            return todoList.todoList(for: priority).count //если нет , то вернем 0, если есть, то вернем кол-во секций
+        }
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { //этот метод вызывается каждый раз, когда тэйблвью нужна целл
@@ -85,7 +76,9 @@ class ChecklistViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
         //let item = todoList.todos[indexPath.row]
-        if let item = tableData[indexPath.section]?[indexPath.row] {
+        if let priority = priorityForSectionIndex(indexPath.section) {
+            let items = todoList.todoList(for: priority) // список айтемов
+            let item = items[indexPath.row] // айтем из списка
             configureText(for: cell, with: item)
             configureCheckmark(for: cell, with: item)
         }
@@ -98,20 +91,32 @@ class ChecklistViewController: UITableViewController {
             return
         }
         if let cell = tableView.cellForRow(at: indexPath) { //возьмет ячейку по индекс патсу
-            let item = todoList.todos[indexPath.row]
-            item.toggleChecked() // метод переключение галочки
-            configureCheckmark(for: cell, with: item)
-            tableView.deselectRow(at: indexPath, animated: true)
+            if let priority = priorityForSectionIndex(indexPath.section) {
+                let items = todoList.todoList(for: priority)
+                let item = items[indexPath.row]
+                item.toggleChecked() // метод переключение галочки
+                configureCheckmark(for: cell, with: item)
+                tableView.deselectRow(at: indexPath, animated: true)
+            }
         }
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-        todoList.todos.remove(at: indexPath.row) // удалили роу в индекс патс. индекс патс изменился, нужно его переназначить (строка ниже).
-        let indexPaths = [indexPath] // tell table what we deleted. держим все возможные индекс патсы([], [], []) ОН ТАМ ОДИН! , которые были изменены верхней строчкой, в массиве. В ЭТОМ МАССИВЕ [indexPath] ОДИН ЭЛЕМЕНТ.
-        tableView.deleteRows(at: indexPaths, with: .automatic) //удаляяем ячейки
-        
-        tableView.reloadData()
+        if let priority = priorityForSectionIndex(indexPath.section) {
+            let item = todoList.todoList(for: priority)[indexPath.row]
+            todoList.remove(item, from: priority, at: indexPath.row) // удалили роу в индекс патс. индекс патс изменился, нужно его переназначить (строка ниже).
+            let indexPaths = [indexPath] // tell table what we deleted. держим все возможные индекс патсы([], [], []) ОН ТАМ ОДИН! , которые были изменены верхней строчкой, в массиве. В ЭТОМ МАССИВЕ [indexPath] ОДИН ЭЛЕМЕНТ.
+            tableView.deleteRows(at: indexPaths, with: .automatic) //удаляяем ячейки
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) { //этот метод позволяет двигать РЯДЫ
+        if let srcPriority = priorityForSectionIndex(sourceIndexPath.section),
+           let destPriority = priorityForSectionIndex(destinationIndexPath.section) {
+            let item = todoList.todoList(for: srcPriority)[sourceIndexPath.row]
+            todoList.move(item: item, from: srcPriority, at: sourceIndexPath.row, to: destPriority, at: destinationIndexPath.row)
+        }
+        tableView.reloadData() //важная штука, которая сразу же обновляет всю вью и дату после перемещения дела в другую секцию.
     }
     
     func configureText(for cell: UITableViewCell, with item: CheckListItem) { // изменение текста в ячейке
@@ -140,8 +145,11 @@ class ChecklistViewController: UITableViewController {
         } else if segue.identifier == "EditItemSegue" { //setup our EditItem segue
             if let itemDetailViewController = segue.destination as? ItemDetailViewController {
                 if let cell = sender as? UITableViewCell, //кастим через Sender ячейку до ТейблВью цел.
-                    let indexPath = tableView.indexPath(for: cell) { //можем получить indexPath от tableView ячейки через cell выше. Мы узнали куда ткнул user.
-                    let item = todoList.todos[indexPath.row] //get checklist item from todolist.
+                    let indexPath = tableView.indexPath(for: cell),
+                    let priority = priorityForSectionIndex(indexPath.section)
+                    { //можем получить indexPath от tableView ячейки через cell выше. Мы узнали куда ткнул user.
+                        
+                    let item = todoList.todoList(for: priority)[indexPath.row] //get checklist item from todolist.
                     itemDetailViewController.itemToEdit = item //add item to itemDetailViewController and set it to property .itemToEdit. НЕ ПОНЯТНО!
                 } // 1)get the item that is being edited from the "sender: Any?". Cast a sender to a type of tableViewCell.
                 itemDetailViewController.delegate = self
@@ -151,19 +159,24 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-       return tableData.count
+       return TodoList.Priority.allCases.count
     }
     
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return UILocalizedIndexedCollation.current().sectionTitles //string of actual title of the section
-    }
-    
-    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        return UILocalizedIndexedCollation.current().section(forSectionIndexTitle: index) //сделать индекс каждой секции (0-25)
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return UILocalizedIndexedCollation.current().sectionTitles[section] // буквы алфавита от A-Z
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? { 
+        var title: String? = nil
+        if let priority = priorityForSectionIndex(section) {
+            switch priority {
+            case .high:
+                title = "High Priority Todos"
+            case .medium:
+                title = "Medium Priority Todos"
+            case .low:
+                title = "Low Priority Todos"
+            case .no:
+                title = "Someday Todo Items"
+            }
+        }
+        return title
     }
 }
 
@@ -175,20 +188,25 @@ extension ChecklistViewController: ItemDetailViewControllerDelegate {//update th
     func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: CheckListItem) {
         navigationController?.popViewController(animated: true) //pop the view controller of the stack
         //we need to become delegate of our AddItemTableViewController by using Segues:
-        let rowIndex = todoList.todos.count - 1  //кол-во дел в нашем списке на данный момент
+        let rowIndex = todoList.todoList(for: .medium).count - 1  //кол-во дел в нашем списке на данный момент
          //add checklist item to our todo list
-        let indexPath = IndexPath(row: rowIndex, section: 0) //rowIndex - это например 6
+        let indexPath = IndexPath(row: rowIndex, section: TodoList.Priority.medium.rawValue) //rowIndex - это например 6
         let indexPaths = [indexPath] // тогда [[6, 0]]
         tableView.insertRows(at: indexPaths, with: .automatic) //вставляем наш новый айтем в основной чеклиствьюконтроллер следующей строчкой.
     }
     
     func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: CheckListItem) {
-        if let index = todoList.todos.firstIndex(of: item) {//index айтема в модели ,например [6]
-            let indexPath = IndexPath(row: index, section: 0) //например, [6,0]
-            if let cell = tableView.cellForRow(at: indexPath) {// получить целл , которая на данном локейшне.
-                configureText(for: cell, with: item) //метод изменения текста.
+        
+        for priority in TodoList.Priority.allCases {
+            let currentList = todoList.todoList(for: priority)
+            if let index = currentList.firstIndex(of: item) { //проверяем есть ли айтем в массиве, если есть, то возвращаем его индекс
+                let indexPath = IndexPath(row: index, section: priority.rawValue) //например, [6,0]. путь к индексу в зависимости от секции
+                if let cell = tableView.cellForRow(at: indexPath) {
+                    configureText(for: cell, with: item) //метод изменения текста.
+                }
+                
             }
-        }
+        } //узнать в каком прайорити находятся айтемы из чеклиста
         navigationController?.popViewController(animated: true)
     }
 }
--- Checklist/TodoList.swift
+++ Checklist/TodoList.swift
@@ -10,7 +10,14 @@ import Foundation
 
 class TodoList {
     
-    var todos: [CheckListItem] = []
+    enum Priority: Int, CaseIterable { //CaseItirable - we can through all of the variable cases.(можно перебрать все переменные)
+        case high, medium, low, no
+    }
+    
+    private var highPriorityTodos: [CheckListItem] = [] //делаем приватными, чтобы только TodoList класс имел к ним доступ.
+    private var mediumPriorityTodos: [CheckListItem] = []
+    private var lowPriorityTodos: [CheckListItem] = []
+    private var noPriorityTodos: [CheckListItem] = []
     
     init() {
         
@@ -26,37 +33,92 @@ class TodoList {
         row3Item.text = "Code an app"
         row4Item.text = "Watch a movie"
         
-        todos.append(row0Item)
-        todos.append(row1Item)
-        todos.append(row2Item)
-        todos.append(row3Item)
-        todos.append(row4Item)
+        addTodo(row0Item, for: .low)
+        addTodo(row1Item, for: .high)
+        addTodo(row2Item, for: .medium)
+        addTodo(row3Item, for: .high)
+        addTodo(row4Item, for: .no)
     }
+    
+    
+    func addTodo(_ item: CheckListItem, for priority: Priority, at index: Int = -1) { //удобный способ добавления нового элемента в чеклист. установили индекс -1(это когда пустой список), чтобы не сломать приложуху
+        switch priority {
+        case .high:
+            if index < 0 {
+            highPriorityTodos.append(item)
+            } else {
+                highPriorityTodos.insert(item, at: index)
+            }
+        case .medium:
+            if index < 0 {
+                mediumPriorityTodos.append(item)
+            } else {
+                mediumPriorityTodos.insert(item, at: index)
+            }
+        case .low:
+            if index < 0 {
+                lowPriorityTodos.append(item)
+            } else {
+                lowPriorityTodos.insert(item, at: index)
+            }
+        case .no:
+            if index < 0 {
+                noPriorityTodos.append(item)
+            } else {
+                noPriorityTodos.insert(item, at: index)
+            }
+        }
+    }
+    
+    func todoList(for priority: Priority) -> [CheckListItem] {
+        switch priority {
+        case .high:
+            return highPriorityTodos
+        case .medium:
+            return mediumPriorityTodos
+        case .low:
+            return lowPriorityTodos
+        case .no:
+            return noPriorityTodos
+        }
+    }//на выходе список элементов
     
     func newTodo() -> CheckListItem {
         let item = CheckListItem()
         item.text = randomTitle()
         item.checked = true
-        todos.append(item)
-        
+        mediumPriorityTodos.append(item)//по дефолту все дела mediumPriorityTodos
         return item
     }
     
-    func move(item: CheckListItem, to index: Int) { //берет чеклист айтем и индекс куда подвинуть айтем
-        guard let currentIndex = todos.firstIndex(of: item) else { //index of - метод в который вставляем айтем. else если айтема там нет, то делаем ретерн
-            return // теперь есть локация ОТКУДА И КУДА идет наш айтем.
+    func move(item: CheckListItem, from sourcePriority: Priority, at sourceIndex: Int, to destinationPriority: Priority, at destinationIndex: Int) {
+        
+        remove(item, from: sourcePriority, at: sourceIndex)
+        addTodo(item, for: destinationPriority, at: destinationIndex)
+        
+        
+    }//берем айтем из предыдушей секции по индексу, вставляем в новую секцию по индексу
+    
+    func remove(_ item: CheckListItem, from priority: Priority, at index: Int) {
+        switch priority {
+        case .high:
+            highPriorityTodos.remove(at: index)
+        case .medium:
+            mediumPriorityTodos.remove(at: index)
+        case .low:
+            lowPriorityTodos.remove(at: index)
+        case .no:
+            noPriorityTodos.remove(at: index)
         }
-        todos.remove(at: currentIndex) //  убираем из текущего индекса
-        todos.insert(item, at: index) // берем что? - Айтем и вставляем по индексу
     }
     
-    func remove(items: [CheckListItem]) {//берем массив из чеклист айтемов, т.е. их местоположение [ [],[],[] ]
-        for item in items {
-            if let index = todos.firstIndex(of: item) {//todos.index(of: item) - берет индекс айтема, который находится в массиве сверху, например [0] (1ый элемент) или [3] (четвертый)
-                todos.remove(at: index) //убираем элемент(ячейку) по индексу
-            }
-        }
-    }
+//    func remove(items: [CheckListItem]) {//берем массив из чеклист айтемов, т.е. их местоположение [ [],[],[] ]
+//        for item in items {
+//            if let index = todos.firstIndex(of: item) {//todos.index(of: item) - берет индекс айтема, который находится в массиве сверху, например [0] (1ый элемент) или [3] (четвертый)
+//                todos.remove(at: index) //убираем элемент(ячейку) по индексу
+//            }
+//        }
+//    }
     
     private func randomTitle() -> String { //добавление рандомного заголовка в новую ячейку
         var titles = ["New todo item", "Generic todo", "Fill me out", "I need something to do", "Much todo about nothing"]
